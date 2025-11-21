5b2a7f86c393897ba420fd3ae49902d61b1da85c
shivanshu-kashyap-09 <kashyapshivanshu63@gmail.com>
2025-11-21 20:35:23 +0530

    connect-routes

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,37 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ServiceCard from '../components/card/ServiceCard';
+import { useParams } from "react-router-dom";
 
 const Service = () => {
   const [apiServices, setApiServices] = useState([]);
+  const { eventName } = useParams();
+  const decodedEventName = decodeURIComponent(eventName);
+  console.log(decodedEventName);
+
+  const [eventService, setEventService] = useState([]);
+  const [serviceVendor, setServiceVendor] = useState({});
+
+  const handleEvent = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/event/service/get/by/${decodedEventName}`);
+      if (response.status === 200) {
+        console.log(response.data.services);
+        
+        setEventService(response.data.services);
+      }
+    } catch (error) {
+      console.error("Error occured in fetching the event service : " + error);
+    }
+  }
+
+  const handleServices = async () => {
+    try {
+      for (let i = 0; i < eventService.length; i++) {
+        const serviceName = eventService[i];
+
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/service-option/get/by/serivce/${serviceName}`
+        );
+
+        if (response.status === 200) {
+          console.log(response.data);
+          
+          setServiceVendor(prev => [...prev, response.data]);
+        }
+      }
+    } catch (error) {
+      console.error("Error in getting service vendors: ", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/service-option/get/all");
+  if (eventService.length > 0) {
+    handleServices();
+  }
+}, [eventService]);
 
-        const mapped = response.data.map(item => ({
-          id: item._id,
-          title: item.serviceName,
-          tagline: item.shortDesc,
-          vendor: {
-            name: item.companyName,
-            description: item.longDesc,
-            rating: item.rating,
-            price: `₹ ${item.price}`,
-            rawPrice: item.price,
-            imageUrl: item.images?.[0] || "",
-          }
-        }));
 
-        setApiServices(mapped);
-      } catch (error) {
-        console.error("Error fetching services:", error);
-      }
-    };
 
-    fetchData();
-  }, []);
+  useEffect(() => {
+    handleEvent();
+  }, [])
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-100 via-white to-blue-100 py-12 pb-0 font-sans">
